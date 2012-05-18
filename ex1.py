7cb6e6b3be4550ff8d80d1e584aa48d3c26d6e8d
unknown <Tim Bradley@lenovo.(none)>
2012-05-18 18:26:42 +0100

    first commit

---
+++ ex1.py
@@ -0,0 +1,9 @@
+print "Hello world"
+print "hello again"
+print "I like typing this"
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+#print "hello again"

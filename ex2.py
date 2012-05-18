4e3df066b1efcc9662a97e22adc4786f8833b8cb
unknown <Tim Bradley@lenovo.(none)>
2012-05-18 18:34:19 +0100

    second

---
+++ ex2.py
@@ -0,0 +1,10 @@
+#Comments can go here
+
+print "Hello world" #Or they can go here, at the end of a line
+print "Hello again"
+print "I like typing this" 
+
+#You can comment out code to disable it
+
+#print "This wont run"
+print "This will run"
